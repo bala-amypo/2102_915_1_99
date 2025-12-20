1232c536333e19e5f804bff72ee9eb0fdc29174d
bala-amypo <bala@amypo.in>
2025-12-20 14:39:12 +0000

    User commit

---
+++ src/main/test/java/com/example/demo/FullIntegrationTest.java
@@ -0,0 +1,10 @@
+package com.example.demo;
+
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
+import org.testng.annotations.Listeners;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@Listeners(TestResultListener.class)
+public class FullIntegrationTest extends AbstractTestNGSpringContextTests {
+}
