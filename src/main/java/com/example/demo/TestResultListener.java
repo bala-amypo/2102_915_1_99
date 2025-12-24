de79da6fdcd5f9e8ea253ca417b98c540cefe586
bala-amypo <bala@amypo.in>
2025-12-24 15:26:37 +0000

    User commit

--- src/main/java/com/example/demo/TestResultListener.java
+++ src/main/java/com/example/demo/TestResultListener.java
@@ -1,15 +1,45 @@
 // src/main/java/com/example/demo/TestResultListener.java
 package com.example.demo;
 
-import org.testng.*;
+import org.testng.ITestContext;
+import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class TestResultListener implements ITestListener {
-    // Keep minimal and non-intrusive; tests only need the class present
-    @Override public void onTestStart(ITestResult result) {}
-    @Override public void onTestSuccess(ITestResult result) {}
-    @Override public void onTestFailure(ITestResult result) {}
-    @Override public void onTestSkipped(ITestResult result) {}
-    @Override public void onTestFailedButWithinSuccessPercentage(ITestResult result) {}
-    @Override public void onStart(ITestContext context) {}
-    @Override public void onFinish(ITestContext context) {}
+
+    @Override
+    public void onTestStart(ITestResult result) {
+        System.out.println("[TEST START] " + result.getName());
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+        System.out.println("[TEST PASSED] " + result.getName());
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+        System.out.println("[TEST FAILED] " + result.getName() +
+                " | Exception: " + result.getThrowable());
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+        System.out.println("[TEST SKIPPED] " + result.getName());
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+        System.out.println("[TEST PARTIAL SUCCESS] " + result.getName());
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+        System.out.println("[TEST CONTEXT START] " + context.getName());
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+        System.out.println("[TEST CONTEXT FINISH] " + context.getName());
+    }
 }
