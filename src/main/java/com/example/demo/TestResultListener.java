2ec71351c8566d73d3400748098eb27c97090b87
bala-amypo <bala@amypo.in>
2025-12-24 03:22:44 +0000

    User commit

--- src/main/java/com/example/demo/TestResultListener.java
+++ src/main/java/com/example/demo/TestResultListener.java
@@ -1,7 +1,36 @@
 package com.example.demo;
 
+import org.testng.ITestContext;
 import org.testng.ITestListener;
+import org.testng.ITestResult;
 
 public class TestResultListener implements ITestListener {
-    // Intentionally empty; required by hidden test harness
+
+    @Override
+    public void onTestStart(ITestResult result) {
+    }
+
+    @Override
+    public void onTestSuccess(ITestResult result) {
+    }
+
+    @Override
+    public void onTestFailure(ITestResult result) {
+    }
+
+    @Override
+    public void onTestSkipped(ITestResult result) {
+    }
+
+    @Override
+    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+    }
+
+    @Override
+    public void onStart(ITestContext context) {
+    }
+
+    @Override
+    public void onFinish(ITestContext context) {
+    }
 }
