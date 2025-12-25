ca4a8467d757aeb5e310fdeb780e77b6b130f616
bala-amypo <bala@amypo.in>
2025-12-25 05:21:09 +0000

    User commit

--- src/main/java/com/example/demo/TestResultListener.java
+++ src/main/java/com/example/demo/TestResultListener.java
@@ -1,45 +1,22 @@
-// src/main/java/com/example/demo/TestResultListener.java
 package com.example.demo;
-
-import org.testng.ITestContext;
+ 
 import org.testng.ITestListener;
 import org.testng.ITestResult;
-
+ 
 public class TestResultListener implements ITestListener {
-
-    @Override
-    public void onTestStart(ITestResult result) {
-        System.out.println("[TEST START] " + result.getName());
-    }
-
+ 
     @Override
     public void onTestSuccess(ITestResult result) {
-        System.out.println("[TEST PASSED] " + result.getName());
+        System.out.println(result.getMethod().getMethodName() + " - PASS");
     }
-
+ 
     @Override
     public void onTestFailure(ITestResult result) {
-        System.out.println("[TEST FAILED] " + result.getName() +
-                " | Exception: " + result.getThrowable());
+        System.out.println(result.getMethod().getMethodName() + " - FAIL");
     }
-
+ 
     @Override
     public void onTestSkipped(ITestResult result) {
-        System.out.println("[TEST SKIPPED] " + result.getName());
-    }
-
-    @Override
-    public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
-        System.out.println("[TEST PARTIAL SUCCESS] " + result.getName());
-    }
-
-    @Override
-    public void onStart(ITestContext context) {
-        System.out.println("[TEST CONTEXT START] " + context.getName());
-    }
-
-    @Override
-    public void onFinish(ITestContext context) {
-        System.out.println("[TEST CONTEXT FINISH] " + context.getName());
+        System.out.println(result.getMethod().getMethodName() + " - SKIP");
     }
 }
